8d7e2feed2f4df31c16b7da72632f06e563e1fa4
sercantimocin <sercantimocin@hotmail.com>
2017-09-24 16:14:47 +0300

    added

---
+++ NewUserHandler/Tracker.cs
@@ -0,0 +1,57 @@
+﻿namespace NewUserHandler
+{
+    using System;
+    using System.Configuration;
+    using System.Data;
+    using System.Data.SqlClient;
+
+    public class Tracker
+    {
+        private readonly string localDbCon;
+
+        public Tracker()
+        {
+            this.localDbCon = ConfigurationManager.ConnectionStrings["LocalDb"].ConnectionString;
+
+            SqlDependency.Stop(this.localDbCon);
+            SqlDependency.Start(this.localDbCon);
+        }
+
+        public void TrackMessages()
+        {
+
+            using (SqlConnection connection = new SqlConnection(this.localDbCon))
+            {
+                SqlCommand command = new SqlCommand("SELECT Name, Surname FROM dbo.Users", connection);
+
+                SqlDependency dependency = new SqlDependency(command);
+
+                dependency.OnChange += this.OnDependencyChange;
+
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    // Process the DataReader.
+                }
+            }
+        }
+
+        private void OnDependencyChange(object sender, SqlNotificationEventArgs e)
+        {
+            Console.WriteLine(e.Info);
+            Console.WriteLine(e.Source);
+            Console.WriteLine(e.Type);
+
+            this.TrackMessages();
+        }
+
+        ~Tracker()
+        {
+            SqlDependency.Stop(this.localDbCon);
+        }
+    }
+}
---
+++ dml_commands.sql
@@ -0,0 +1,6 @@
+﻿
+truncate table Users
+
+insert into Users (Name,Surname) values ('ali','duru3')
+
+update Users set name='selim' where Id=5
